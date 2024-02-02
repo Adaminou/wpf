f01d44e6b538bef0eabbe68c54b08b23e2c0f51c
Essalhi Adam <200537@CCM-Asty-Moulin.local>
2024-02-02 16:01:00 +0100

    pafini

---
+++ D/ACT7_COURSE_ADAM6TI/Pari.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ACT7_COURSE_ADAM6TI
+{
+     class Pari
+    {
+        int _montant;
+        int _numChien;
+        Parieur _joueur;
+
+      
+
+        public int Montant
+        {
+            get { return _montant; }
+            set { _montant = value; }
+        }
+
+        public int NumChien
+        {
+            get { return _numChien; }
+            set { _numChien = value; }
+        }
+
+        public Parieur Joueur
+        {
+            get { return _joueur; }
+            set { _joueur = value; }
+        }
+
+
+        public Pari(int montant, int numChien, Parieur joueur)
+        {
+            _montant = montant;
+           _numChien = numChien;
+            _joueur = joueur;
+        }
+
+        public void GetDescription(TextBlock txtInfos)
+        {
+
+        }
+        public void PrixFinal(int numGagnant)
+        {
+
+        }
+       
+    }
+
+}
---
+++ D/ACT7_COURSE_ADAM6TI/Parieur.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ACT7_COURSE_ADAM6TI
+{
+    class Parieur
+    {
+        string _nom;
+        Pari _monPari;
+        int _cash;
+        TextBlock _textBlockEtatPari;
+
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+
+        public Pari MonPari
+        {
+            get { return _monPari; }
+            set { _monPari = value; }
+        }
+       
+        public int Cash
+
+        {
+            get { return _cash; }
+            set { _cash = value; }
+        }
+        public TextBlock TextBlockEtatPari
+        {
+            get { return _textBlockEtatPari; }
+            set { _textBlockEtatPari = value; }
+        }
+
+        public Parieur(string nom, Pari monPari, int cash, TextBlock textBlockEtatPari)
+        {
+            _nom = nom;
+            _monPari = monPari;
+            _cash = cash;
+            _textBlockEtatPari = textBlockEtatPari;
+
+        }
+        public void Pari(int valeurPari, int numChien)
+        {
+            
+        }
+        public void ResetPari()
+        {
+
+        }
+        public void MajInfos()
+        {
+
+        }
+
+    }
+    
+}
